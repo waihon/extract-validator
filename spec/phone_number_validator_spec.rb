a56231c3decd4bcb085a4180a51de00dab4875fc
Waihon Yew <yewwaihon@gmail.com>
2021-03-14 07:05:36 +0000

    Validate that a person is invalid if a phone number formatting is incorrect

--- spec/phone_number_validator_spec.rb
+++ spec/phone_number_validator_spec.rb
@@ -14,4 +14,12 @@ describe PhoneNumberFormatValidator do
 
     expect(valid_person).to be_valid
   end
+
+  it "does not validate if a phone number formatting is incorrect" do
+    invalid_person_one = TestPerson.new(phone_number: "++7777-777-777")
+    invalid_person_two = TestPerson.new(phone_number: "777-7777-7777")
+
+    expect(invalid_person_one).not_to be_valid
+    expect(invalid_person_two).not_to be_valid
+  end
 end

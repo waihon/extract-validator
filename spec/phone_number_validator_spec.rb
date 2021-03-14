56a3c45d3a7c339935c6bebcf37ae76e4b4bdf3f
Waihon Yew <yewwaihon@gmail.com>
2021-03-14 06:41:15 +0000

    Validate that a person is valid if no phone number is passed

--- spec/phone_number_validator_spec.rb
+++ spec/phone_number_validator_spec.rb
@@ -8,4 +8,10 @@ class TestPerson
   validates :phone_number, phone_number_format: true
 end
 
-  
+describe PhoneNumberFormatValidator do
+  it "validates if no phone number is passed" do
+    valid_person = TestPerson.new(phone_number: nil)
+
+    expect(valid_person).to be_valid
+  end
+end
